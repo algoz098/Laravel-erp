a80ac7bdd3c82958fe8842a21ed439555fb4554c
Guilherme <guilherme_giannasi@hotmail.com>
2017-02-20 22:38:44 -0300

    Adicionado novas funcionalidades ao ticket

--- resources/views/tickets/novo.blade.php
+++ resources/views/tickets/novo.blade.php
@@ -2,28 +2,36 @@
 @section('content')
   <div class="panel panel-default">
     <div class="panel-heading"><i class="fa fa-book"></i> Adicionar Ticket</div>
-    <form method="POST" action="{{url('novo/tickets')}}/{{$ticket->id}}/edit">
+    @if(isset($ticket))
+      <form method="POST" action="{{url('novo/tickets')}}/{{$ticket->id}}/edit">
+    @else
+      <form method="POST" action="{{url('novo/tickets')}}">
+    @endif
       {{csrf_field()}}
       <div class="panel-body">
         <div class="row" id="secondNavbar">
           <div class="col-md-4 pull-right text-right">
-            <a href="{{url('lista/tickets')}}" class="btn btn-warning"><i class="fa fa-book"></i> Voltar a lista</a>
-            <a href="{{url('novo/tickets')}}" class="btn btn-warning"><i class="fa fa-book"></i> Voltar a seleção</a>
-            <button class="btn btn-success"><i class="fa fa-check"></i> Salvar</button>
+            @botaoLista(tickets*fa-book)
+            @botaoSalvar
           </div>
         </div>
         <div class="row">
           <div class="col-md-3">
             <div class="panel panel-default">
               <div class="panel-body">
-                <div class="form-group">
-                  <label for="contato">Contato:</label>
-                  <input type="text" value="{{$ticket->contato->nome}}" disabled class="form-control" id="contato">
-                </div>
+                @if (isset($ticket))
+                  @selecionaContato(Contato:*$ticket->contato->id*$ticket->contato->nome)
+                @else
+                  @selecionaContato(Contato:)
+                @endif
                 <div class="form-group">
                   <label for="Status">Estado:</label>
-                  <select class="form-control" name="status" id="status">
-                    <option selected value="{{$ticket->status}}">{{$ticket->status}}</option>
+                  <select class="form-control" name="status" id="status" >
+                    @if (isset($ticket))
+                      <option value="{{$ticket->status}}" selected>{{$ticket->status}} (Atual)</option>
+                    @else
+                      <option selected>Aberto</option>
+                    @endif
                     <option value="Aberto">Aberto</option>
                     <option value="Respondido">Respondido</option>
                     <option value="Em processo">Em processo</option>
@@ -40,7 +48,7 @@
                 <div class="form-group">
                   <label for="Descricao">Descrição:</label>
                   <textarea  name="descricao">
-                    {!!$ticket->descricao!!}
+                    {{isset($ticket->descricao)?$ticket->descricao:""}}
                   </textarea>
                 </div>
               </div>
